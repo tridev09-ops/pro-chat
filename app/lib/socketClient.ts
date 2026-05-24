2d643a923bc02e6c539dbc5c33b809d07be9f593
tridev09-ops <tridev17848@gmail.com>
2026-05-24 20:06:09 +0530

    create seprate socket server

--- app/lib/socketClient.ts
+++ app/lib/socketClient.ts
@@ -1,6 +1,11 @@
 import { io } from "socket.io-client";
 
-// This is created once and reused everywhere
-const socket = io();
+const socketServerUrl = process.env.SOCKET_SERVER_URL || "http://localhost:4000";
+
+// Establish the connection
+const socket = io(socketServerUrl, {
+  reconnection: true,
+  transports: ["websocket"], // Force websocket for faster connection
+});
 
 export default socket;
